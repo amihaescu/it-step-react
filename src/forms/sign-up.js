9c1baf2a69d89434371746440537747e2a6006ea
Andrei_Mihaescu <andrei.mihaescu@dell.com>
2020-11-15 11:29:14 +0200

    Added refs

--- src/forms/sign-up.js
+++ src/forms/sign-up.js
@@ -1,27 +1,29 @@
+import React from 'react';
+
 export default function SignUpForm() {
+
+    let email = React.createRef()
+    let password = React.createRef()
+    let optIn = React.createRef()
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        let email = document.getElementById('exampleInputEmail1').value
-        let password = document.getElementById('exampleInputPassword1').value
-        let optIn = document.getElementById('exampleCheck1').checked
-
-        console.log(`Signed up with ${email} and ${password}. User wants to recieved updates: ${optIn}`);
+        console.log(`Signed up with ${email.current.value} and ${password.current.value}. User wants to recieved updates: ${optIn.current.checked}`);
     }
     return (
         <form onSubmit={handleSubmit} className="col-6">
             <h2>Sign up</h2>
             <div className="form-group">
                 <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={email} />
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <input type="password" className="form-control" id="exampleInputPassword1" ref={password} />
             </div>
             <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" ref={optIn} />
                 <label className="form-check-label" htmlFor="exampleCheck1">Subscribe to promotional updates</label>
             </div>
             <button type="submit" className="btn btn-primary">Sign up</button>
