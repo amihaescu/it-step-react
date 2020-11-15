40112aaa6ad14f897fcc6d0515ccddccf7c946ba
Andrei_Mihaescu <andrei.mihaescu@dell.com>
2020-11-15 12:45:37 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import './App.css';
 import CommentList from './components/class';
 import SignUpForm from './forms/sign-up';
 import LoginForm from './forms/login';
+import Navbar from './navbar/navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Switch from 'react-bootstrap/esm/Switch';
+import Main from './components/main';
 function App() {
 
   return (
     <div className="App container">
+
       {/* <CommentList></CommentList> */}
       {/* <SignUpForm></SignUpForm> */}
-      <LoginForm></LoginForm>
+      {/* <LoginForm></LoginForm> */}
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/signup" component={SignUpForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/comments" component={CommentList} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (
+            <h1>Welcome to our mini FB built with React</h1>
+        )
+    }
+}
